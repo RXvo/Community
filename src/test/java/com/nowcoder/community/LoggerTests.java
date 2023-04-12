90d57afd15401185b34cb0d17bafc64723b600b6
Rxov <1286623452@qq.com>
2023-04-12 16:10:25 +0800

    首次提交

--- src/test/java/com/nowcoder/community/LoggerTests.java
+++ src/test/java/com/nowcoder/community/LoggerTests.java
@@ -22,7 +22,7 @@ public class LoggerTests {
         logger.debug("debug log");
         logger.info("info log");
         logger.warn("warn log");
-        logger.error("error log");
+//        logger.error("error log");
     }
 
 }
